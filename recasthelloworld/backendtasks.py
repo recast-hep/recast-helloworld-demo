070734744ca71bb7c4e010ccc761c59808f2d443
Lukas Heinrich <lukas.heinrich@cern.ch>
2015-03-31 20:08:13 +0200

    socketlog

--- recasthelloworld/backendtasks.py
+++ recasthelloworld/backendtasks.py
@@ -2,12 +2,16 @@ from celery import shared_task
 
 import datetime
 
+from recastbackend.backendtasks import socketlog
+
 @shared_task
 def helloWorld(jobguid):
     workdir = 'workdirs/{}'.format(jobguid)
     with open('{}/helloWorld.txt'.format(workdir),'w') as f:
       f.write('Hello World! {}'.format(str(datetime.datetime.now())))
+    socketlog(jobguid,'helloWorld done')
     return jobguid
+
   
 def resultlist():
   return ['helloWorld.txt']
